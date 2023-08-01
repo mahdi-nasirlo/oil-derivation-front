d6741c9be4ff7746e1318aaa0e7b19964bd28895
morteza-vahdati <72885979+mahdi-nasirlo@users.noreply.github.com>
2023-08-01 10:55:51 +0330

    use ant design theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import StyledComponentsRegistry from '../../lib/AntdRegistry'
+import theme from '../../theme/themeConfig';
+import { Button, ConfigProvider } from '../../lib/antd';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +19,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <StyledComponentsRegistry>
+          <ConfigProvider theme={theme}>
+            {children}
+          </ConfigProvider>
+        </StyledComponentsRegistry>
+      </body>
     </html>
   )
 }
---
+++ theme/themeConfig.ts
@@ -0,0 +1,22 @@
+import type { ThemeConfig } from 'antd';
+
+const theme: ThemeConfig = {
+  token: {
+    fontSize: 16,
+    colorPrimary: "#18948a",
+    colorTextBase: "#171923",
+    colorBgBase: "#ffffff",
+    borderRadius: 10,
+    wireframe: true,
+    colorBgContainer: "#ffffff",
+    colorBgElevated: "#ffffff",
+    colorBgLayout: "#ffffff",
+    colorBgSpotlight: "#ffffff",
+    colorBgMask: "#ffffff",
+    colorSuccess: "#31c41a",
+    colorInfo: "#6366f1",
+    colorPrimaryBg: "#f2f2f2"
+  }
+};
+
+export default theme;
