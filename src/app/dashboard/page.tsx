a630acb0d15f3d1bea846c4e27b0f9b922852d55
mahdi-nasirlo <72885979+mahdi-nasirlo@users.noreply.github.com>
2023-08-14 16:03:49 +0330

    fix middleware tasks

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,22 @@
 'use client'
 import {Button} from "../../../lib/antd";
+import {useEffect, useState} from "react";
 
 
 export default function Home() {
+    const [state, setState] = useState()
+
+    useEffect(() => {
+        const res = fetch(`${process.env['NEXT_PUBLIC_API_URL']}/security/getToken`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authentication': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySWQiOiIxIiwibmJmIjoxNjkyMDExMDc1LCJleHAiOjE3NTUxNjk0NzUsImlhdCI6MTY5MjAxMTA3NSwiaXNzIjoiTXlXZWJzaXRlLmNvbSJ9.dyjPzqDy4h9V4t-Pbl6Vr0r7b6x56Y6OO_k1dwhpbhg`
+            },
+        })
+        
+    })
+
     return (
         <>
             <Button type="primary">
@@ -11,3 +25,18 @@ export default function Home() {
         </>
     )
 }
+
+// async function getData() {
+//     const res = await fetch(`${process.env['NEXT_PUBLIC_API_URL']}/security/getId`, {
+//         headers: {Authentication: `Bearer `}
+//     })
+//     // The return value is *not* serialized
+//     // You can return Date, Map, Set, etc.
+//
+//     if (!res.ok) {
+//         // This will activate the closest `error.js` Error Boundary
+//         throw new Error('Failed to fetch data')
+//     }
+//
+//     return res.json()
+// }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,79 @@
-import {chain} from "../middleware/chine";
-import {redirectMiddleware} from "../middleware/redirect";
-import {authenticateMiddleware} from "../middleware/authenticate";
+import {NextRequest, NextResponse} from "next/server";
 
-export default chain([authenticateMiddleware, redirectMiddleware])
+export default async function middleware(request: NextRequest) {
 
+    const pathname = request.nextUrl.pathname
 
+    const hasApiToken = request.cookies.has('accessToken')
 
+    const params = request.nextUrl.searchParams
 
+    console.log(pathname.startsWith('/dashboard'), pathname.startsWith('/auth'), pathname === '/', hasApiToken, request.nextUrl.searchParams.has('code'))
 
+    if (pathname.startsWith('/dashboard') && !hasApiToken) {
+        console.log('/auth/login')
+        return NextResponse.redirect(new URL('/auth/login', request.url))
+    }
 
+    if (pathname.startsWith('/auth') && hasApiToken) {
+        console.log('/dashboard')
+        return NextResponse.redirect(new URL('/dashboard', request.url));
+    }
 
+    if (pathname === '/') {
+        console.log("/")
+        return NextResponse.redirect(new URL('/dashboard', request.url));
+    }
+
+    if (request.nextUrl.searchParams.has('code')) {
+
+        const token = await getToken(request, params.get('code') || '', request.nextUrl.origin + request.nextUrl.pathname);
+
+        if (token) {
+
+            const response = NextResponse.redirect(new URL('/dashboard', request.url));
+
+            response.cookies.set("accessToken", token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV !== "development",
+                sameSite: "strict",
+                maxAge: 60 * 60,
+                // path: request.url,
+            });
+
+            return response;
+        }
+
+    }
+}
+
+export const config = {
+    matcher: ['/auth/:path*', '/dashboard/:path*', '/'],
+}
+
+async function getToken(request: NextRequest, code: string, redirectUrl: string) {
+
+    let data = {
+        "code": code,
+        "RedirectUri": redirectUrl
+    }
+
+    // console.log(data)
+
+    const response = await fetch(`${process.env['NEXT_PUBLIC_API_URL']}/security/getToken`, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+        throw new Error(`API request failed with status: ${response.status}`);
+    }
+
+    const responseData = await response.json();
+
+    console.log(responseData.data?.access_token)
+    return responseData.data?.access_token
+}
