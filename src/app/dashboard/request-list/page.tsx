4c2d8472bcecefed6d9324c6946b8d21a85c00d0
morteza-vahdati <mvahdati1382@gmail.com>
2023-08-19 16:56:37 +0330

    added table to request-list

---
+++ src/app/dashboard/request-list/page.tsx
@@ -0,0 +1,298 @@
+"use client";
+
+import {
+  Button,
+  Col,
+  Form,
+  FormItemProps,
+  Input,
+  Row,
+  Select,
+  Space,
+  Table,
+  Typography
+} from "../../../../lib/antd";
+import React from "react";
+import Link from "next/link";
+import { ColumnsType } from "antd/es/table";
+import { Pagination, Tag } from 'antd';
+
+
+export default function Page() {
+  return (
+    <>
+      <div className="box-border w-full mt-4 p-6">
+        <Form name="form_item_path" layout="vertical">
+          <MyFormItemGroup prefix={["user"]}>
+            <MyFormItemGroup prefix={["name"]}>
+              <Row gutter={32}>
+                <Col span={12}>
+                  <MyFormItem name="year-establishment" label="نام محصول ">
+                    <Input size="large" />
+                  </MyFormItem>
+                </Col>
+                <Col span={12}>
+                  <MyFormItem name="lastName" label=" وضعیت فعالیت">
+                    <Select size="large" />
+                  </MyFormItem>
+                </Col>
+              </Row>
+              <Row dir="ltr">
+                <Col span={2}>
+                  <div className="flex gap-6 ">
+                    <Button
+                      className="w-full management-info-form-submit btn-delete-filter"
+                      size="large"
+                      type="primary"
+                    >
+                      <span className="flex gap-3 justify-center ">
+                        حذف فیلتر
+                      </span>
+                    </Button>
+                    <Button
+                      className="w-full management-info-form-submit btn-filter"
+                      size="large"
+                      type="primary"
+                    >
+                      <span className="flex gap-3 justify-center ">
+                        اعمال فیلتر
+                      </span>
+                    </Button>
+                  </div>
+                </Col>
+              </Row>
+            </MyFormItemGroup>
+          </MyFormItemGroup>
+        </Form>
+      </div>
+      <Table pagination={false} className='mt-8' columns={columns} dataSource={data} />
+      <div className="flex flex-row-reverse justify-end items-center">
+        <Pagination
+          defaultCurrent={1}
+          total={120}
+          defaultPageSize={10}
+          hideOnSinglePage
+          className="flex flex-row-reverse justify-end my-4"
+        />
+        <Typography className="font-normal text-xs">تعداد نمایش ردیف در صفحه</Typography>
+      </div>
+    </>
+  );
+}
+
+interface DataType {
+  key: string;
+  Row: number;
+  ProductName: string;
+  TrackingCode: string;
+  ConfirmedRequestCode: string;
+  DateRegistration: string;
+  ActivityStatus: string[];
+}
+
+const columns: ColumnsType<DataType> = [
+  {
+    title: "ردیف",
+    dataIndex: "Row",
+    key: "1",
+  },
+  {
+    title: "نام محصول",
+    dataIndex: "ProductName",
+    key: "2",
+  },
+  {
+    title: "کد رهگیری",
+    dataIndex: "TrackingCode",
+    key: "3",
+  },
+  {
+    title: "کد درخواست تایید شده",
+    dataIndex: "ConfirmedRequestCode",
+    key: "4",
+  },
+  {
+    title: "تاریخ ثبت",
+    dataIndex: "DateRegistration",
+    key: "5",
+  },
+  {
+    title: "وضعیت فعالیت",
+    dataIndex: "ActivityStatus",
+    key: "6",
+    render: (_, { ActivityStatus }) => (
+      <>
+        {ActivityStatus.map((ActivityStatus) => {
+          let color = ActivityStatus.length > 16 ? 'geekblue' : 'green';
+          if (ActivityStatus === 'loser') {
+            color = 'volcano';
+          }
+          return (
+            <Tag color={color} key={ActivityStatus}>
+              {ActivityStatus.toUpperCase()}
+            </Tag>
+          );
+        })}
+      </>
+    ),
+  },
+
+  {
+    title: "جزئیات",
+    key: "جزئیات",
+    render: (_, record) => (
+      <Space size="middle">
+        <Link href={""} className="action-btn-delete">
+          حذف
+        </Link>
+      </Space>
+    ),
+  },
+];
+
+
+const MyFormItemContext = React.createContext<(string | number)[]>([]);
+
+interface MyFormItemGroupProps {
+  prefix: string | number | (string | number)[];
+  children: React.ReactNode;
+}
+
+function toArr(
+  str: string | number | (string | number)[]
+): (string | number)[] {
+  return Array.isArray(str) ? str : [str];
+}
+
+const MyFormItemGroup = ({ prefix, children }: MyFormItemGroupProps) => {
+  const prefixPath = React.useContext(MyFormItemContext);
+  const concatPath = React.useMemo(
+    () => [...prefixPath, ...toArr(prefix)],
+    [prefixPath, prefix]
+  );
+
+  return (
+    <MyFormItemContext.Provider value={concatPath}>
+      {children}
+    </MyFormItemContext.Provider>
+  );
+};
+
+const MyFormItem = ({ name, ...props }: FormItemProps) => {
+  const prefixPath = React.useContext(MyFormItemContext);
+  const concatName =
+    name !== undefined ? [...prefixPath, ...toArr(name)] : undefined;
+
+  return <Form.Item name={concatName} {...props} />;
+};
+
+const data: DataType[] = [
+  {
+    key: "1",
+    Row: 1,
+    ProductName: "نفتا",
+    TrackingCode: "351665168",
+    ConfirmedRequestCode: "321",
+    DateRegistration: '1400/01/01',
+    ActivityStatus: ["دریافت استاندارد"],
+  },
+  {
+    key: "2",
+    Row: 2,
+    ProductName: "بنزین پیرولیز",
+    TrackingCode: "351665168",
+    ConfirmedRequestCode: "546",
+    DateRegistration: '1400/01/01',
+    ActivityStatus: ["عدم دریافت استاندارد"],
+  },
+  {
+    key: "3",
+    Row: 3,
+    ProductName: "آروماتیک سنگین",
+    TrackingCode: "351665168",
+    ConfirmedRequestCode: "988",
+    DateRegistration: '1400/01/01',
+    ActivityStatus: ["در حال آزمایش"],
+  },
+  {
+    key: "4",
+    Row: 4,
+    ProductName: "بنزین پیرولیز",
+    TrackingCode: "351995168",
+    ConfirmedRequestCode: "546",
+    DateRegistration: '1400/01/01',
+    ActivityStatus: ["عدم دریافت استاندارد"],
+  },
+  {
+    key: "5",
+    Row: 5,
+    ProductName: "آروماتیک سنگین",
+    TrackingCode: "351165168",
+    ConfirmedRequestCode: "988",
+    DateRegistration: '1400/01/01',
+    ActivityStatus: ["در حال آزمایش"],
+  },
+  {
+    key: "6",
+    Row: 6,
+    ProductName: "بنزین پیرولیز",
+    TrackingCode: "351605168",
+    ConfirmedRequestCode: "546",
+    DateRegistration: '1400/01/01',
+    ActivityStatus: ["عدم دریافت استاندارد"],
+  },
+  {
+    key: "7",
+    Row: 7,
+    ProductName: "آروماتیک سنگین",
+    TrackingCode: "351665768",
+    ConfirmedRequestCode: "988",
+    DateRegistration: '1400/01/01',
+    ActivityStatus: ["در حال آزمایش"],
+  },
+  {
+    key: "8",
+    Row: 8,
+    ProductName: "بنزین پیرولیز",
+    TrackingCode: "351645168",
+    ConfirmedRequestCode: "546",
+    DateRegistration: '1400/01/01',
+    ActivityStatus: ["عدم دریافت استاندارد"],
+  },
+  {
+    key: "9",
+    Row: 9,
+    ProductName: "آروماتیک سنگین",
+    TrackingCode: "351865168",
+    ConfirmedRequestCode: "988",
+    DateRegistration: '1400/01/01',
+    ActivityStatus: ["در حال آزمایش"],
+  },
+  {
+    key: "10",
+    Row: 10,
+    ProductName: "بنزین پیرولیز",
+    TrackingCode: "351662168",
+    ConfirmedRequestCode: "988",
+    DateRegistration: '1400/01/01',
+    ActivityStatus: ["در حال آزمایش"],
+  },
+  {
+    key: "11",
+    Row: 11,
+    ProductName: "آروماتیک سنگین",
+    TrackingCode: "351665168",
+    ConfirmedRequestCode: "546",
+    DateRegistration: '1400/01/01',
+    ActivityStatus: ["عدم دریافت استاندارد"],
+  },
+  {
+    key: "12",
+    Row: 12,
+    ProductName: "بنزین پیرولیز",
+    TrackingCode: "351665168",
+    ConfirmedRequestCode: "988",
+    DateRegistration: '1400/01/01',
+    ActivityStatus: ["در حال آزمایش"],
+  },
+];
--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -1,12 +1,12 @@
 'use client'
 import Link from "next/link";
 import React from 'react';
-import type {MenuProps} from 'antd';
-import {Badge, Menu} from 'antd';
+import type { MenuProps } from 'antd';
+import { Badge, Menu } from 'antd';
 import Image from 'next/image';
-import {usePathname} from "next/navigation";
-import {ChatBubbleLeftEllipsisIcon, Cog6ToothIcon, HomeIcon, PencilIcon} from "@heroicons/react/24/outline";
-import {ListBulletIcon} from "@heroicons/react/24/solid";
+import { usePathname } from "next/navigation";
+import { ChatBubbleLeftEllipsisIcon, Cog6ToothIcon, HomeIcon, PencilIcon } from "@heroicons/react/24/outline";
+import { ListBulletIcon } from "@heroicons/react/24/solid";
 
 
 export default function LayoutSidebar() {
@@ -17,7 +17,7 @@ export default function LayoutSidebar() {
 
             <Menu
                 defaultSelectedKeys={[pathname]}
-                style={{width: "272px", padding: "0 16px", paddingTop: "40px"}}
+                style={{ width: "272px", padding: "0 16px", paddingTop: "40px" }}
                 className='px-4'
                 mode="inline"
                 items={items}
@@ -53,33 +53,44 @@ const MakeRequest = () =>
         <Link href="/dashboard/request/personnel-info">
             ثبت درخواست
         </Link>
-        <Badge style={{marginLeft: "1px", boxShadow: "none"}} count={5}/>
+        <Badge style={{ marginLeft: "1px", boxShadow: "none" }} count={5} />
     </div>
 </>)
 
-export const SvgIcon = ({src, width = 16, height = 16, className}:
-                            { src: string, width?: number, height?: number, className?: string }) => <Image src={src}
-                                                                                                            width={width}
-                                                                                                            height={height}
-                                                                                                            className={className}
-                                                                                                            alt=''/>
+
+const RequestList = () =>
+(<>
+    <div className='flex justify-between items-center'>
+        <Link href="/dashboard/request-list">
+            لیست درخواست
+        </Link>
+    </div>
+</>)
+
+
+export const SvgIcon = ({ src, width = 16, height = 16, className }:
+    { src: string, width?: number, height?: number, className?: string }) => <Image src={src}
+        width={width}
+        height={height}
+        className={className}
+        alt='' />
 
 const items: MenuProps['items'] = [
 
 
-    getItem(<Link href="/dashboard">خانه</Link>, '/dashboard', <HomeIcon width={16} height={16}/>),
+    getItem(<Link href="/dashboard">خانه</Link>, '/dashboard', <HomeIcon width={16} height={16} />),
 
-    {type: 'divider'},
+    { type: 'divider' },
 
     getItem('پیشخوان', 'dashboard', null, [
-        getItem(<MakeRequest/>, "/dashboard/request/personnel-info", <PencilIcon width={16} height={16}/>),
-        getItem('لیست درخواست', 'request_list', <ListBulletIcon width={16} height={16}/>)
+        getItem(<MakeRequest />, "/dashboard/request/personnel-info", <PencilIcon width={16} height={16} />),
+        getItem(<RequestList />, 'request_list', <ListBulletIcon width={16} height={16} />)
     ], 'group'),
 
-    {type: 'divider'},
+    { type: 'divider' },
 
     getItem('مدیریت', 'management', null, [
-        getItem("تنظیمات حساب کاربری", 'profile-setting', <Cog6ToothIcon width={16} height={16}/>),
-        getItem('ارتباط با پشتیبان', 'connect-support', <ChatBubbleLeftEllipsisIcon width={16} height={16}/>)
+        getItem("تنظیمات حساب کاربری", 'profile-setting', <Cog6ToothIcon width={16} height={16} />),
+        getItem('ارتباط با پشتیبان', 'connect-support', <ChatBubbleLeftEllipsisIcon width={16} height={16} />)
     ], 'group'),
 ];
