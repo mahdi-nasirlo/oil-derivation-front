abb34da6b700df7d1cbbc69b133c3a644e67c28d
mahdi-nasirlo <72885979+mahdi-nasirlo@users.noreply.github.com>
2023-08-21 11:58:26 +0330

    add api fetch functions

---
+++ units/RequestMaster/createRequestMaster.ts
@@ -0,0 +1,26 @@
+import axios, {AxiosResponse} from "axios";
+import {RequestMaster} from "@/app/dashboard/request/production-process/page";
+
+export async function createRequestMaster(data: RequestMaster) {
+    try {
+        const res: AxiosResponse = await axios.post(
+            "http://192.168.52.102:97/api/RequestMaster/Create",
+            data
+        );
+
+        const requestMasterUid = res.data.data;
+
+        // Set the cookie with the requestMasterUid
+        setCookie("requestMasterUid", requestMasterUid, 7); // Set the expiry as desired
+
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+// Function to set a cookie
+function setCookie(name: string, value: string, days: number) {
+    const expires = new Date();
+    expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
+    document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/`;
+}
