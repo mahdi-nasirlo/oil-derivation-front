35ecd35802c69fc3191ded3e373f014a1ffe5b50
mahdi-nasirlo <72885979+mahdi-nasirlo@users.noreply.github.com>
2023-08-23 09:56:30 +0330

    check mistake

--- src/app/dashboard/request/production-process/page.tsx
+++ src/app/dashboard/request/production-process/page.tsx
@@ -4,99 +4,101 @@ import {Button, Col, Divider, Form, Input, Row, Typography, Upload,} from "../..
 import React from "react";
 import {UploadOutlined} from "@ant-design/icons";
 import {useRouter} from "next/navigation";
-import {createRequestMaster} from "../../../../../units/RequestMaster/createRequestMaster";
 import {getCookie, setCookie} from "cookies-next";
+import {createRequestMaster} from "../../../../../units/RequestMaster/createRequestMaster";
 
 export default function Page() {
-  const router = useRouter();
+    const router = useRouter();
 
-  const onFinish = (values: RequestMasterForm) => {
+    const onFinish = (values: RequestMasterForm) => {
 
-    setCookie("processDescription", values.processDescription)
+        let data: RequestMaster = {
+            ...values,
+            fileName: values.fileName?.file.name,
+        };
 
-    router.push("/dashboard/request/laboratory")
+        setCookie("processDescription", values.processDescription)
 
-    let data: RequestMaster = {
-      ...values,
-      fileName: values.fileName.file.name,
+        createRequestMaster(data, () => {
+            router.push("/dashboard/request/formulacion");
+        })
+
+        router.push("/dashboard/request/laboratory")
+        
     };
 
-    return createRequestMaster(data, () => {
-      router.push("/dashboard/request/formulacion");
-    });
-  };
+    const onFinishFailed = (errorInfo: any) => {
+        console.log("Failed:", errorInfo);
+    };
 
-  const onFinishFailed = (errorInfo: any) => {
-    console.log("Failed:", errorInfo);
-  };
-
-  return (
-    <>
-      <Typography className="text-right font-medium text-base">
-        لطفا اطلاعات خواسته شده را با دقت وارد نمایید.
-      </Typography>
-      <Divider />
-      <Form
-          initialValues={{
-            processDescription: getCookie("processDescription") || ""
-          }}
-        onFinishFailed={onFinishFailed}
-        onFinish={onFinish}
-        name="form_item_path"
-        layout="vertical"
-      >
-        <Row gutter={32}>
-          <Col span={24}>
-            <Form.Item rules={[{required: true, message: "لطفا فیلد را وارد نمایید"}]} name="processDescription" label="شرح فرآیند تولید">
-              <Input.TextArea
-                maxLength={100}
-                style={{
-                  height: 120,
-                  resize: "none",
+    return (
+        <>
+            <Typography className="text-right font-medium text-base">
+                لطفا اطلاعات خواسته شده را با دقت وارد نمایید.
+            </Typography>
+            <Divider/>
+            <Form
+                initialValues={{
+                    processDescription: getCookie("processDescription") || ""
                 }}
-                placeholder="وارد کنید"
-              />
-            </Form.Item>
-          </Col>
-        </Row>
-        <Row className="mt-3" gutter={32}>
-          <Col span={24}>
-            <Form.Item name="fileName" label="نمودار شماتیک فرآیند">
-              <Upload
-                multiple={false}
-                action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-                listType="picture"
-                className="w-full"
-              >
-                <Button icon={<UploadOutlined />}>بارگزاری نمایید</Button>
-              </Upload>
-            </Form.Item>
-          </Col>
-        </Row>
-        <Divider />
-          <div className="flex gap-6">
-            <Button
-                className="w-full management-info-form-submit btn-filter"
-                size="large"
-                type="primary"
-                htmlType="submit"
+                onFinishFailed={onFinishFailed}
+                onFinish={onFinish}
+                name="form_item_path"
+                layout="vertical"
             >
+                <Row gutter={32}>
+                    <Col span={24}>
+                        <Form.Item rules={[{required: true, message: "لطفا فیلد را وارد نمایید"}]}
+                                   name="processDescription" label="شرح فرآیند تولید">
+                            <Input.TextArea
+                                maxLength={100}
+                                style={{
+                                    height: 120,
+                                    resize: "none",
+                                }}
+                                placeholder="وارد کنید"
+                            />
+                        </Form.Item>
+                    </Col>
+                </Row>
+                <Row className="mt-3" gutter={32}>
+                    <Col span={24}>
+                        <Form.Item name="fileName" label="نمودار شماتیک فرآیند">
+                            <Upload
+                                multiple={false}
+                                action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                                listType="picture"
+                                className="w-full"
+                            >
+                                <Button icon={<UploadOutlined/>}>بارگزاری نمایید</Button>
+                            </Upload>
+                        </Form.Item>
+                    </Col>
+                </Row>
+                <Divider/>
+                <div className="flex gap-6">
+                    <Button
+                        className="w-full management-info-form-submit btn-filter"
+                        size="large"
+                        type="primary"
+                        htmlType="submit"
+                    >
           <span className="flex gap-3 justify-center ">
             ذخیره و ادامه
           </span>
-            </Button>
-          </div>
-      </Form>
-    </>
-  );
+                    </Button>
+                </div>
+            </Form>
+        </>
+    );
 }
 
 export type RequestMaster = {
-  processDescription: string;
-  fileName: string;
+    processDescription: string;
+    fileName: string;
 };
 
 type RequestMasterForm = {
-  processDescription: string;
-  fileName: { file: { name: string } };
+    processDescription: string;
+    fileName: { file: { name: string } };
 };
