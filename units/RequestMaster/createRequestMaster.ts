75fb311942fcd1874ccabbc09f9c4bad4d6343cc
mahdi-nasirlo <72885979+mahdi-nasirlo@users.noreply.github.com>
2023-08-23 06:56:48 +0330

    fix create request response action

--- units/RequestMaster/createRequestMaster.ts
+++ units/RequestMaster/createRequestMaster.ts
@@ -1,7 +1,7 @@
 import axios, {AxiosResponse} from "axios";
 import {RequestMaster} from "@/app/dashboard/request/production-process/page";
 
-export async function createRequestMaster(data: RequestMaster) {
+export async function createRequestMaster(data: RequestMaster, callback: any) {
     try {
         const res: AxiosResponse = await axios.post(
             `${process.env["NEXT_PUBLIC_API_URL"]}/api/RequestMaster/Create`,
@@ -13,6 +13,9 @@ export async function createRequestMaster(data: RequestMaster) {
         // Set the cookie with the requestMasterUid
         setCookie("requestMasterUid", requestMasterUid, 7); // Set the expiry as desired
 
+        if (res.data.success) {
+            callback()
+        }
     } catch (error) {
         console.error("Error:", error);
     }
