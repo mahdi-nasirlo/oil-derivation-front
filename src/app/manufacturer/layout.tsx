c166317e1697eb632afb44b26a23190d237bfe61
mahdi-nasirlo <72885979+mahdi-nasirlo@users.noreply.github.com>
2023-08-26 09:12:58 +0330

    add home link in manufacturer panel

--- src/app/manufacturer/layout.tsx
+++ src/app/manufacturer/layout.tsx
@@ -2,44 +2,52 @@
 
 import AppLayout from "@/components/layout/layout";
 import React from "react";
-import { getMenuItem } from "@/components/layout/sidebar";
+import {getMenuItem} from "@/components/layout/sidebar";
 import Link from "next/link";
-import {
-  Bars3Icon,
-  ChatBubbleLeftEllipsisIcon,
-  InformationCircleIcon,
-} from "@heroicons/react/24/outline";
-import { MenuProps } from "antd";
+import {Bars3Icon, HomeIcon, InformationCircleIcon,} from "@heroicons/react/24/outline";
+import {MenuProps} from "antd";
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
+                                       children,
+                                   }: {
+    children: React.ReactNode;
 }) {
-  return (
-    <>
-      <AppLayout sidebarItems={items}>{children}</AppLayout>
-    </>
-  );
+    return (
+        <>
+            <AppLayout sidebarItems={items}>{children}</AppLayout>
+        </>
+    );
 }
 
 const items: MenuProps["items"] = [
-  getMenuItem(
-    "پنل رییس اجرایی",
-    "management",
-    null,
-    [
-      getMenuItem(
-        <Link href={"/manufacturer/list"}> لیست تولید کننده ها</Link>,
-        "msnufscturer-list",
-        <Bars3Icon width={16} height={16} />
-      ),
-      getMenuItem(
-        <Link href={"/manufacturer/info"}>اطلاعات تولید کننده ها</Link>,
-        "msnufscturer-info",
-        <InformationCircleIcon width={16} height={16} />
-      ),
-    ],
-    "group"
-  ),
+
+    getMenuItem(
+        <Link href="/manufacturer">خانه</Link>,
+        "/manufacturer",
+        <HomeIcon width={16} height={16}/>
+    ),
+
+    {type: "divider"},
+
+    getMenuItem(
+        "پنل رییس اجرایی",
+        "management",
+        null,
+        [
+
+            getMenuItem(
+                <Link href={"/manufacturer/list"}> لیست تولید کننده ها</Link>,
+                "msnufscturer-list",
+                <Bars3Icon width={16} height={16}/>
+            ),
+
+            getMenuItem(
+                <Link href={"/manufacturer/info"}>اطلاعات تولید کننده ها</Link>,
+                "msnufscturer-info",
+                <InformationCircleIcon width={16} height={16}/>
+            ),
+
+        ],
+        "group"
+    ),
 ];
