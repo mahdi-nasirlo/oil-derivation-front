85314aa5ce99652573ac681af843e42c9cb859e5
morteza-vahdati <mvahdati1382@gmail.com>
2023-09-10 13:15:59 +0330

    seprated the AddRawMaterial to diffrents component

---
+++ src/app/admin-pannel/adding-raw-material/components/primary-add-raw-material-form.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { Button, Col, Form, Row, Select } from 'antd'
+import React from 'react'
+
+export default function PrimaryAddRawMaterialForm() {
+
+    return (
+        <div className="box-border w-full p-6">
+            <Form name="form_item_path" layout="vertical">
+                <Row gutter={[16, 16]}>
+                    <Col xs={24} md={12}>
+                        <Form.Item
+                            name="year-establishment"
+                            label="نام ماده اولیه"
+                        >
+                            <Select size="large" placeholder="انتخاب کنید" />
+                        </Form.Item>
+                    </Col>
+                </Row>
+                <Row dir="ltr">
+                    <Col xs={10} md={3} lg={2}>
+                        <div className="flex gap-4">
+                            <Button
+                                className="btn-filter"
+                                size="large"
+                                type="primary"
+                                htmlType="submit"
+                            >
+                                اعمال فیلتر
+                            </Button>
+                            <Button
+                                className="btn-delete-filter"
+                                size="large"
+                                type="primary"
+                                htmlType="submit"
+                            >
+                                حذف فیلتر
+                            </Button>
+                        </div>
+                    </Col>
+                </Row>
+            </Form>
+        </div>)
+}
