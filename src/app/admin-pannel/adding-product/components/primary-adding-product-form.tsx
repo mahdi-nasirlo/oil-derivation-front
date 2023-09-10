ab2676382df4c44ce36d37b06d0cdd4d57212a81
morteza-vahdati <mvahdati1382@gmail.com>
2023-09-10 15:24:22 +0330

    seprated the Adding Product to diffrents component

---
+++ src/app/admin-pannel/adding-product/components/primary-adding-product-form.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+
+import { Button, Col, Form, Input, Row, Select } from 'antd'
+import React from 'react'
+
+export default function PrimaryAddingProductForm() {
+
+    return (
+        <div className="box-border w-full p-6">
+            <Form name="form_item_path" layout="vertical">
+                <Row gutter={[16, 16]}>
+                    <Col xs={24} md={12}>
+                        <Form.Item name="year-establishment" label="نام محصول ">
+                            <Input size="large" placeholder="وارد کنید" />
+                        </Form.Item>
+                    </Col>
+                    <Col xs={24} md={12}>
+                        <Form.Item name="lastName" label="کد">
+                            <Input size="large" placeholder="وارد کنید" />
+                        </Form.Item>
+                    </Col>
+                </Row>
+                <Row gutter={[16, 16]}>
+                    <Col xs={24} md={12}>
+                        <Form.Item name="year-establishment" label=" دانسیته">
+                            <Select size="large" placeholder="انتخاب کنید" />
+                        </Form.Item>
+                    </Col>
+                    <Col xs={24} md={12} />
+                </Row>
+                <Row dir="ltr">
+                    <Col xs={10} md={3} lg={2}>
+                        <div className="flex gap-4">
+                            <Button
+                                className="btn-filter"
+                                size="large"
+                                type="primary"
+                                htmlType="submit"
+                            >
+                                اعمال فیلتر
+                            </Button>
+                            <Button
+                                className="btn-delete-filter"
+                                size="large"
+                                type="primary"
+                                htmlType="submit"
+                            >
+                                حذف فیلتر
+                            </Button>
+                        </div>
+                    </Col>
+                </Row>
+            </Form>
+        </div >
+    )
+}
